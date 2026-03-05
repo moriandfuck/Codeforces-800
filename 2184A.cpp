31e8778be6e1779a03fb6f6bc46ad64c53ce9037
sisifuse <zch816243020@outlook.com>
2026-03-05 20:07:40 +0800

    要多想想，在多想想......有点搞的，我只看到了一种可能，下意识忽略了另一种，思路要更明确！！！

---
+++ 2184A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n == 2 || n == 3)
+            cout << n << '\n';
+        else if (n % 2 == 0)
+            cout << 0 << '\n';
+        else
+            cout << 1 << '\n';
+    }
+    return 0;
+}
