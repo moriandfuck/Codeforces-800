21d0f07519abc41ec3894b142cef330ff91f1b09
sisifuse <zch816243020@outlook.com>
2026-03-12 21:05:01 +0800

    签到题

---
+++ 1985A.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+int main(int argc,char const* argv[])
+{
+    int t;
+    std::cin>>t;//1~100
+    while(t--)
+    {
+        std::string a,b;
+        std::cin>>a>>b;
+        char temp_contaner=0;
+        if(a[0]!=b[0])
+        {
+            temp_contaner=a[0];
+            a[0]=b[0];
+            b[0]=temp_contaner;
+        }
+        std::cout<<a<<" "<<b<<std::endl;
+        
+        
+    }
+    return 0;
+}
