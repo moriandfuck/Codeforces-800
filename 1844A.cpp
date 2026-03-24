a7a931c0e9f83b43a8a819d796c6e62f578d54eb
sisifuse <zch816243020@outlook.com>
2026-03-24 11:07:35 +0800

    减法游戏应该也是很经典的贪心题目了，只是我取巧了，而且题目要求也很松，所以不难做，如果更难的话就是费脑子的贪心了

---
+++ 1844A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+int main(int argc,char const* argv[])
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin>>t;//1~100
+    while(t--)
+    {
+        int a,b;
+        std::cin>>a>>b;
+        std::cout<<a+b<<std::endl;
+    }
+}
+
